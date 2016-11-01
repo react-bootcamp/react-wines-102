d90836a18e8f5789a6ce0dda5fd4bf92f50050d1
Mathieu <mathieu.ancelin@gmail.com>
2016-11-01 11:05:42 +0100

    Publish new static version

--- src/components/WineApp.js
+++ src/components/WineApp.js
@@ -9,18 +9,21 @@ export const WineApp = React.createClass({
     this.context.router.goBack();
   },
   render() {
+    const displayButton = window.location.pathname === '/' || window.location.pathname === '/react-wines-102/'
+      ? false
+      : true;
     return (
       <div className="container">
         <h1 className="center-align">Open Wine Database</h1>
         <div className="center-align">
           You can read the Wines API documentation at <a href="https://wines-api.herokuapp.com" target="_blank">https://wines-api.herokuapp.com</a> and try it <a href="http://petstore.swagger.io/?url=https://react-bootcamp.github.io/react-wines/swagger.json" target="_blank">here</a>
         </div>
-        <div className="center-align" style={{ marginTop: 20 }}>
+        {displayButton && (<div className="center-align" style={{ marginTop: 20 }}>
           <button className="btn waves-effect waves-light" onClick={this.goBack} type="button">
             <i className="material-icons left">fast_rewind</i>
             Back
           </button>
-        </div>
+        </div>)}
         <div className="row">
           {this.props.children}
         </div>
